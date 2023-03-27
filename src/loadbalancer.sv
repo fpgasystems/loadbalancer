aa799c6822540de9586eb1102ab36c6393ced4f6
Hongyu <hongyuhe.cs@googlemail.com>
2023-03-27 18:20:49 +0200

    Add initial implementation of LB logic

--- src/loadbalancer.sv
+++ src/loadbalancer.sv
@@ -15,15 +15,17 @@ module loadbalancer #(
     input logic aresetn,
     
     // * Load balancer is on the slave side relative to the HTTP module.
-    // ? How can I change the data width of the interface while specifying the modport `s`?
-    AXI4S.s meta_snk,
-    AXI4S.s hdr_snk,
-    AXI4S.s bdy_snk,
+    AXI4S.s meta_in,
+    AXI4S.s hdr_in,
+    AXI4S.s bdy_in,
+
+    AXI4S.m meta_out,
 
     input logic [N_REGIONS*2*OPERATOR_ID_WIDTH-1:0] region_stats_in,
 
-    output logic[32-1:0] lb_ctrl,
-    output logic[32-1:0] pr_ctrl
+    output logic [$clog2(N_REGIONS)-1:0] lb_ctrl
+    // output logic[($clog2(N_REGIONS)+OPERATOR_ID_WIDTH)-1:0] pr_ctrl
+    
 );
     /** Meta queue
         * Push a request into the queue on every posedge. (not considering the queue is full)
@@ -31,7 +33,6 @@ module loadbalancer #(
     */
     logic meta_val_src;
     logic meta_rdy_src;
-    logic meta_data_received;
     logic [HTTP_META_WIDTH-1:0] meta_data;
 
     queue_stream #(
@@ -41,9 +42,9 @@ module loadbalancer #(
         .aclk(aclk),
         .aresetn(aresetn),
         // * Enqueue
-        .rdy_snk(meta_snk.tready),
-        .val_snk(meta_snk.tvalid),
-        .data_snk(meta_snk.tdata),
+        .rdy_snk(meta_in.tready),
+        .val_snk(meta_in.tvalid),
+        .data_snk(meta_in.tdata),
         // * Dequeue
         .val_src(meta_val_src),
         .rdy_src(meta_rdy_src),
@@ -51,14 +52,13 @@ module loadbalancer #(
     );
 
     // TODO: LB may not be able to process data as fast as it receives.
-    assign meta_rdy_src = ~meta_queue.inst_fifo.is_empty;
+    // assign meta_rdy_src = ~meta_queue.inst_fifo.is_empty;
 
     // always_ff @( posedge aclk ) begin : manage_meta_queue
         // if (aresetn == 1'b0) begin
         //     meta_data_received <= 1'b0;
         // end
 
-        // ? How can I read once and set 
         // if (!meta_data_received) begin
         //     meta_rdy_src <= 1'b1;
         //     // if (meta_queue.val_src && !meta_queue.inst_fifo.is_empty) begin
@@ -74,49 +74,90 @@ module loadbalancer #(
         //     meta_data_received <= 1'b0;
         // end
     // end
-    
-    // * Interface with the HTTP module 
-    // * {meta_meta, method, hdr, bdy, oid}
-    logic [HTTP_METHOD_WIDTH-1:0] http_method;
-    logic has_headers;
-    logic has_body;
-    logic [OPERATOR_ID_WIDTH-1:0] operator_id;
-    assign http_method = meta_data[HTTP_META_META_WIDTH +: HTTP_METHOD_WIDTH];
-    assign has_headers = meta_data[HTTP_META_META_WIDTH+HTTP_METHOD_WIDTH];
-    assign has_body = meta_data[HTTP_META_META_WIDTH+HTTP_METHOD_WIDTH+1];
-    assign operator_id = meta_data[HTTP_META_WIDTH-1 -: OPERATOR_ID_WIDTH];
 
     /** Pull status of all regions.
     */    
-    logic[N_REGIONS-1:0][OPERATOR_ID_WIDTH*2-1:0] region_stats;
+    localparam integer LOAD_BITS = $clog2(QDEPTH);
+    logic [N_REGIONS-1:0][(OPERATOR_ID_WIDTH+LOAD_BITS)-1:0] region_stats;
 
     always_ff @( posedge aclk ) begin : upate_region_status
-        if (aresetn == 1'b01) begin
+        if (aresetn == 1'b0) begin
             int region;
             for (region = 0; region < N_REGIONS; region=region+1) begin
                 region_stats[region] = 0;
             end
         end else begin
-            int region;
-            for (region = 0; region < N_REGIONS; region=region+1) begin
-                region_stats[region] <= region_stats_in[region*2 +: OPERATOR_ID_WIDTH];
-            end
+            region_stats <= region_stats_in;
         end
     end
 
     /** Load balancing logic. 
-    */     
+    */  
+
     logic [HTTP_META_WIDTH-1:0] meta_data_taken;
+    logic meta_received;
+
+    // * Interface with the HTTP module 
+    logic [HTTP_METHOD_WIDTH-1:0] http_method;
+    logic has_headers;
+    logic has_body;
+    logic [OPERATOR_ID_WIDTH-1:0] requested_oid;
+    // * Little endian: (MSB) {meta_meta, method, hdr, bdy, oid} (LSB)
+    // TODO: Change OID to the last bits.
+    assign requested_oid = meta_data_taken[0 +: OPERATOR_ID_WIDTH];
+    assign has_body = meta_data_taken[OPERATOR_ID_WIDTH];
+    assign has_hdr = meta_data_taken[OPERATOR_ID_WIDTH+1];
+    assign http_method = meta_data_taken[(HTTP_META_WIDTH-1) -: HTTP_META_META_WIDTH];
     
     always_ff @( posedge aclk ) begin : load_balance
         if (aresetn == 1'b0) begin
-            lb_ctrl <= 32'b0;
-            pr_ctrl <= 32'b0;
+            lb_ctrl <= 'hX;
+            meta_received <= 1'b0;
         end
         
-        // TODO: Once reveived a meta (via handshake), send out control signals.
         if (meta_rdy_src && meta_queue.val_src) begin
             meta_data_taken <= meta_data;
+            meta_received <= 1'b1;
+        end else begin
+            meta_received <= 1'b0;
+            meta_data_taken <= 'hXXXXXX;
+        end
+    end
+
+    reg [$clog2(N_REGIONS)-1:0] min_load_vfid = 'hX;
+    reg [OPERATOR_ID_WIDTH-1:0] last_oid;
+    // ? Why assigning a parameter doesn't work?
+    reg [LOAD_BITS-1:0] min_load = QDEPTH;
+    reg [LOAD_BITS-1:0] cur_load;
+    // ? Why doesn't the waveform show the intended behavior of the flag?
+    reg lb_flag = 1'b0;
+
+    integer vfid;
+    always_comb begin : find_min_load
+        // TODO: Replace the constant.
+        min_load = 'b1111111111;
+        lb_flag = 1'b0;
+        for (vfid = 0; vfid < N_REGIONS; vfid=vfid+1) begin
+            // * [ [(region1): oid, load], [(region2): oid, load] ]
+            cur_load = region_stats[vfid][0 +: LOAD_BITS];
+            last_oid = region_stats[vfid][LOAD_BITS +: OPERATOR_ID_WIDTH];
+
+            if (cur_load < min_load) begin
+                min_load = cur_load;
+                min_load_vfid = vfid;
+            end else if (cur_load == min_load && last_oid == requested_oid) begin
+                // * To break ties, prefer a region with requested oid for mitigating cold start
+                min_load_vfid = vfid;
+            end
+        end
+        lb_flag = 1'b1;
+    end
+
+    // always_ff @( posedge aclk, meta_received ) begin
+    always_ff @( posedge aclk, lb_flag) begin
+        if (lb_flag) begin
+            lb_ctrl <= min_load_vfid;
+            meta_rdy_src <= ~meta_queue.inst_fifo.is_empty;
         end
     end
     
--- tests/loadbalancer_tb.sv
+++ tests/loadbalancer_tb.sv
@@ -6,37 +6,43 @@ module loadbalancer_tb ();
     reg clk;
     reg resetn;
 
-    localparam OPERATOR_ID_WIDTH = 16;
+    localparam OPERATOR_ID_WIDTH = 2;
     localparam HTTP_META_WIDTH = 8;
     localparam N_REGIONS = 4;
     localparam QDEPTH = 4;
     localparam PNTR_BITS = $clog2(QDEPTH);
 
     // * Instantiate interfaces.
-    AXI4S #(.AXI4S_DATA_BITS(8)) meta_src(clk);
+    AXI4S #(.AXI4S_DATA_BITS(HTTP_META_WIDTH)) meta_src(clk);
     AXI4S hdr_src(clk);
     AXI4S bdy_src(clk);
+    
+    AXI4S #(.AXI4S_DATA_BITS(HTTP_META_WIDTH)) meta_snk(clk);
 
-    logic [2*OPERATOR_ID_WIDTH-1:0] lb_ctrl;
-    logic [2*OPERATOR_ID_WIDTH-1:0] pr_ctrl;
+    logic  [$clog2(N_REGIONS)-1:0]lb_ctrl;
+    // logic [2*OPERATOR_ID_WIDTH-1:0] pr_ctrl;
 
-    logic [N_REGIONS*OPERATOR_ID_WIDTH-1:0] region_stats_out;
+    logic [N_REGIONS*(OPERATOR_ID_WIDTH+PNTR_BITS)-1:0] region_stats_out;
 
     loadbalancer #(
         .HTTP_META_WIDTH(HTTP_META_WIDTH),
-        .QDEPTH(4)
+        .OPERATOR_ID_WIDTH(OPERATOR_ID_WIDTH),
+        .N_REGIONS(N_REGIONS),
+        .QDEPTH(QDEPTH)
     ) lb (
         .aclk(clk),
         .aresetn(resetn),
-        .meta_snk(meta_src),
-        .hdr_snk(hdr_src),
-        .bdy_snk(bdy_src),
+        .meta_in(meta_src),
+        .hdr_in(hdr_src),
+        .bdy_in(bdy_src),
         .region_stats_in(region_stats_out),
-        .lb_ctrl(lb_ctrl),
-        .pr_ctrl(pr_ctrl)
+        .meta_out(meta_snk),
+        .lb_ctrl(lb_ctrl)
+        // .pr_ctrl(pr_ctrl)
     );
 
-    logic [N_REGIONS*OPERATOR_ID_WIDTH-1:0] region_stats_in;
+    // [ [region1: oid, load], [region2: oid, load] ] -> 4 * (2+2) = 16
+    logic [N_REGIONS-1:0][(OPERATOR_ID_WIDTH+PNTR_BITS)-1:0] region_stats;
     logic [QDEPTH-1:0][HTTP_META_WIDTH-1:0] meta_q_data;
     logic [HTTP_META_WIDTH-1:0] meta_q_in;
     logic [PNTR_BITS-1:0] meta_q_n_entries;
@@ -46,20 +52,20 @@ module loadbalancer_tb ();
     logic meta_q_is_empty;
     logic [HTTP_META_WIDTH-1:0] meta_data_taken;
 
-    assign region_stats_in = lb.region_stats_in;
+    assign region_stats = lb.region_stats;
     
     assign meta_q_data = lb.meta_queue.inst_fifo.data;
     assign meta_q_in = lb.meta_queue.inst_fifo.data_in;
 
-    assign meta_q_src_val = meta_src.tvalid;
+    assign meta_q_val_src = lb.meta_queue.val_src;
     assign meta_q_rdy_snk = lb.meta_queue.rdy_snk;
-    
     assign meta_q_is_full = lb.meta_queue.inst_fifo.is_full;
     assign meta_q_is_empty = lb.meta_queue.inst_fifo.is_empty;
     assign meta_q_n_entries = lb.meta_queue.inst_fifo.n_entries;
     assign meta_q_wr_pntr = lb.meta_queue.inst_fifo.wr_pntr;
     assign meta_q_rd_pntr = lb.meta_queue.inst_fifo.rd_pntr;
 
+    assign meta_out_val = meta_src.tvalid;
     assign meta_rdy_src = lb.meta_rdy_src;
     assign meta_data_taken = lb.meta_data_taken;
 
@@ -72,59 +78,59 @@ module loadbalancer_tb ();
     end
 
     initial begin
+        /** This testbench interacts with the load balancer as an HTTP module. */
+        $display("Testbech::loadbalancer started");
         $dumpfile("lb_tb.vcd");
         $dumpvars(1);
         clk <= 1'b1;
         resetn <= 1'b0;
-        region_stats_out <= 64'hFFFF_FFFF_FFFF_FFFF;
-        $display("Testbech::loadbalancer started");
+        region_stats_out <= 16'h0000;
         
         #2
         resetn <= 1'b1;
+                
         meta_src.tvalid <= 1'b0;
         meta_src.tdata <= 8'hAA;
-        // lb.meta_rdy_src <= 1'b0;
 
         #2
+        region_stats_out <= 16'bXX00_1101_1010_0111;
+
         meta_src.tvalid <= 1'b1;
         meta_src.tdata <= 8'hAA;
-        // lb.meta_rdy_src <= 1'b0;
 
         #2
         meta_src.tvalid <= 1'b0;
         meta_src.tdata <= 8'h99;
-        region_stats_out <= 64'h0123_4567_89AB_CDEF;
-        // lb.meta_rdy_src <= 1'b0;
+        // region_stats_out <= 64'h0123_4567_89AB_CDEF;
 
         #2
+        region_stats_out <= 16'b0010_1100_1011_0110;
         meta_src.tvalid <= 1'b1;
         meta_src.tdata <= 8'hBB;
-        // lb.meta_rdy_src <= 1'b0;
 
         #2
+        region_stats_out <= 16'b0011_1111_1001_0110;
+
         meta_src.tvalid <= 1'b1;
         meta_src.tdata <= 8'hCC;
-        // lb.meta_rdy_src <= 1'b0;
 
         #2
         meta_src.tvalid <= 1'b0;
         meta_src.tdata <= 8'hDD;
-        // lb.meta_rdy_src <= 1'b0;
 
         #2
+        region_stats_out <= 16'b0011_1110_1011_0110;
+
         meta_src.tvalid <= 1'b1;
         meta_src.tdata <= 8'hEE;
-        // lb.meta_rdy_src <= 1'b0;
 
         #2
         meta_src.tvalid <= 1'b1;
         meta_src.tdata <= 8'hFF;
-        // lb.meta_rdy_src <= 1'b0;
 
         #2
         meta_src.tvalid <= 1'b1;
         meta_src.tdata <= 8'hAA;
-        // lb.meta_rdy_src <= 1'b0;
 
         #1
         meta_src.tvalid <= 1'b0;
